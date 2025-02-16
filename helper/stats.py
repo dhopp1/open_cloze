10a79e91906055cab6f5d23c924b7e43c623f82c
dhopp1 <daniel.hopp1@gmail.com>
2025-02-16 17:07:11 +0100

    add stats table

--- helper/stats.py
+++ helper/stats.py
@@ -4,25 +4,26 @@ import streamlit as st
 
 
 def calc_stats():
-    stats = (
+    all_stats = (
         pd.read_csv(
             f"database/{st.session_state['user_id']}/progress.csv", parse_dates=["date"]
         )
-        .loc[
-            lambda x: (x.set == st.session_state["selected_set"])
-            & (x.language == st.session_state["selected_language"]),
-            :,
-        ]
+        .loc[lambda x: x.language == st.session_state["selected_language"], :]
         .reset_index(drop=True)
+    )
+    stats = all_stats.loc[
+        lambda x: (x.set == st.session_state["selected_set"]),
+        :,
+    ].reset_index(drop=True)
+
+    all_sentences = pd.read_csv(
+        f"database/{st.session_state['user_id']}/{st.session_state['language_key'][st.session_state['selected_language']][0]}.csv",
+        parse_dates=["last_practiced"],
     )
 
-    sentences = (
-        pd.read_csv(
-            f"database/{st.session_state['user_id']}/{st.session_state['language_key'][st.session_state['selected_language']][0]}.csv"
-        )
-        .loc[lambda x: (x.set == st.session_state["selected_set"]), :]
-        .reset_index(drop=True)
-    )
+    sentences = all_sentences.loc[
+        lambda x: (x.set == st.session_state["selected_set"]), :
+    ].reset_index(drop=True)
 
     if len(stats) > 0:
         progress_value = stats.set_progress.max()
@@ -163,3 +164,98 @@ def calc_stats():
         )
 
         st.plotly_chart(fig, height=450, use_container_width=True)
+
+    # overall stats
+    overall_progress = (
+        all_sentences.groupby(["set"])[["set", "last_practiced"]]
+        .max()
+        .reset_index(drop=True)
+    )
+    time_spent = (
+        all_stats.groupby("set")
+        .sum(numeric_only=True)
+        .reset_index()[["set", "seconds"]]
+    )
+    overall_progress = overall_progress.merge(time_spent, on="set", how="left")
+
+    set_progress = (
+        all_stats.groupby("set")
+        .max(numeric_only=True)
+        .reset_index()[["set", "set_progress"]]
+    )
+    overall_progress = overall_progress.merge(set_progress, on="set", how="left")
+
+    def get_time(x):
+        try:
+            return int(x)
+        except:
+            return 0
+
+    overall_progress["Minutes studied"] = [
+        get_time(_) for _ in (overall_progress["seconds"] / 60)
+    ]
+
+    all_sentences["been_studied"] = (all_sentences["n_right"] > 0).astype(int)
+    sum_info = (
+        all_sentences.groupby(["set"])[["set", "n_right", "n_wrong", "been_studied"]]
+        .sum(numeric_only=True)
+        .reset_index()
+    )
+    overall_progress = overall_progress.merge(sum_info, on="set", how="left")
+
+    counts = (
+        all_sentences.groupby(["set"])[["set", "n_right"]]
+        .count()
+        .drop(["set"], axis=1)
+        .reset_index()
+        .rename({"n_right": "Number of sentences"}, axis=1)
+    )
+    overall_progress = overall_progress.merge(counts, on="set", how="left")
+
+    overall_progress["Wrong/right ratio"] = round(
+        overall_progress["n_wrong"] / overall_progress["n_right"], 2
+    )
+
+    overall_progress["Avg times studied"] = round(
+        overall_progress["n_right"] / overall_progress["Number of sentences"], 2
+    )
+
+    overall_progress["set_progress"] = [
+        str(_) + "%" if not (pd.isna(_)) else "0%"
+        for _ in round(overall_progress["set_progress"] * 100, 4)
+    ]
+
+    overall_progress["last_practiced"] = [
+        str(_)[:10] if len(str(_)) > 3 else ""
+        for _ in overall_progress["last_practiced"]
+    ]
+
+    # final columns
+    overall_progress = overall_progress.loc[
+        :,
+        [
+            "set",
+            "set_progress",
+            "been_studied",
+            "Number of sentences",
+            "Avg times studied",
+            "Wrong/right ratio",
+            "Minutes studied",
+            "last_practiced",
+        ],
+    ]
+    overall_progress = overall_progress.rename(
+        columns={
+            "set": "Set",
+            "set_progress": "Progress",
+            "been_studied": "Number studied",
+            "last_practiced": "Last practiced",
+        }
+    )
+
+    # display DF
+    st.markdown("### Overall language progress")
+    st.data_editor(
+        overall_progress,
+        hide_index=True,
+    )
